15f4307d7c52a5864b79d86512f675f58cc664e0
Akram <iamakram22@gmail.com>
2024-01-30 17:55:04 +0530

    code improve and finalised

--- script.js
+++ script.js
@@ -1,188 +1,221 @@
-/**
- * Setting options
- */
-let numDigits = parseInt($('#digits').val());
-let numRows = parseInt($('#rows').val());
-let timeInterval = parseInt($('#time').val());
-let includeSubtractions = $('#subtractions').prop('checked');
-$('#settings_save').click(function(){
-    numDigits = parseInt($('#digits').val());
-    numRows = parseInt($('#rows').val());
-    timeInterval = parseInt($('#time').val());
-    includeSubtractions = $('#subtractions').prop('checked');
-});
-
-/**
- * DOM
- */
-const disabledElement = $('.disabled-element');
-const numberContainer = $('#numbers');
-const entry = $('.entries');
-const yourAnswer = $('#yourAnswer');
-const messageContainer = $('#message');
-
-/**
- * Texts
- */
-const correct = 'Correct';
-const incorrect = 'Incorrect';
-
-/**
- * styling
- */
-let controls_height = $('#controls').outerHeight();
-let main_height = 'calc(100svh - ' + controls_height + 'px)';
 $(document).ready(function(){
-    $('#display').css({
-        'height' : main_height
-    });
-});
+    /**
+     * DOM Elements
+     */
+    const disabledElement = $('.disabled-element');
+    const numberContainer = $('#numbers');
+    const entry = $('.entries');
+    const yourAnswer = $('#yourAnswer');
+    const messageContainer = $('#message');
 
-/**
- * Game state management
- */
-let game = {
-    currentIndex: 0,
-    numbers: [],
-    playnum: null,
-    currentSum: 0,
-    entryAdded: false
-};
+    /**
+     * Texts
+     */
+    const correct = 'Correct';
+    const incorrect = 'Incorrect';
 
-/**
- * Generates random numbers based on settings
- */
-function generateRandomNumbers() {
-    game.numbers = [];
-    for (let i = 0; i < numRows; i++) {
-        let number = '';
-        for (let j = 0; j < numDigits; j++) {
-            // Generate a random digit
-            number += Math.floor(Math.random() * 10);
-        }
-        // Include subtractions if checked
-        number = includeSubtractions && Math.random() < 0.5 ? -parseInt(number) : parseInt(number);
+    /**
+     * styling
+     */
+    const controls_height = $('#controls').outerHeight();
+    $('#display').css('height', `calc(100svh - ${controls_height}px)`);
 
-        game.numbers.push(number);
+    
+    /**
+     * Setting options
+     */
+    let settings = {
+        numDigits : parseInt($('#digits').val()),
+        numRows : parseInt($('#rows').val()),
+        timeInterval : parseInt($('#time').val()),
+        includeSubtractions : $('#subtractions').prop('checked'),
+        speakNumbers : $('#speak').prop('checked')
     }
-}
+    $('#settings_save').click(function(){
+        settings.numDigits = parseInt($('#digits').val());
+        settings.numRows = parseInt($('#rows').val());
+        settings.timeInterval = parseInt($('#time').val());
+        settings.includeSubtractions = $('#subtractions').prop('checked');
+        settings.speakNumbers = $('#speak').prop('checked');
+    });
+
+    /**
+     * Validate settings
+     */
+    $('#setting_form').validate();
+
+    /**
+     * Disable settings save or close on invalid
+     */
+    $('#setting_form input').on('input',function() {             
+        const isValid = $(this).valid();
+        $('#settings_save, .btn-close').prop('disabled', !isValid);
+    });
+
+    /**
+     * Game state management
+     */
+    let game = {
+        currentIndex: 0,
+        numbers: [],
+        playnum: null,
+        currentAnswer: 0,
+        entryAdded: false
+    };
+
+    /**
+     * Generates random numbers based on settings
+     */
+    function generateRandomNumbers() {
+        game.numbers = Array.from({ length: settings.numRows }, () => {
+            let number = '';
+            for (let i = 0; i < settings.numDigits; i++) {
+                number += Math.floor(Math.random() * 10);
+            }
+            return settings.includeSubtractions && Math.random() < 0.5 ? -parseInt(number) : parseInt(number);
+        });
+    }
 
 
-/**
- * Updates the current sum of numbers
- */
-function updateCurrentSum() {
-    game.currentSum = game.numbers.reduce((acc, num) => acc + num, 0);
-}
+    /**
+     * Updates the current sum of numbers
+     */
+    function updateCurrentSum() {
+        game.currentAnswer = game.numbers.reduce((acc, num) => acc + num, 0);
+    }
 
-/**
- * Displays numbers
- */
-function displayNumbers() {
-    disabledElement.prop('disabled', true);
-    messageContainer.text('');
-    numberContainer.hide();
-    if (game.currentIndex < game.numbers.length) {
-        numberContainer.fadeIn().text(game.numbers[game.currentIndex]);
-        speak(game.numbers[game.currentIndex]);
-        game.currentIndex++;
-    } else {
+    /**
+     * Displays numbers
+     */
+    function displayNumbers() {
+        beforeShowNumber();
+        if (game.currentIndex < game.numbers.length) {
+            numberContainer.fadeIn().text(game.numbers[game.currentIndex]);
+            game.currentIndex++;
+        } else {
+            clearInterval(game.playnum);
+            afterShowNumber();
+        }
+    }
+
+    /**
+     * Display numbers and speak
+     */
+    function displayNumbersSpeak() {
+        beforeShowNumber();
+        if (game.currentIndex < game.numbers.length) {
+            numberContainer.fadeIn().text(game.numbers[game.currentIndex]);
+            convertSpeech(game.numbers[game.currentIndex]);
+        } else {
+            afterShowNumber();
+        }
+    }
+
+    /**
+     * Modifications before showing number
+     */
+    function beforeShowNumber() {
+        disabledElement.prop('disabled', true);
+        messageContainer.text('');
+        numberContainer.hide();
+    }
+
+    /**
+     * Modification after showing number
+     */
+    function afterShowNumber() {
         numberContainer.fadeIn().text('?');
-        clearInterval(game.playnum);
         disabledElement.prop('disabled', false);
         yourAnswer.focus();
         
         // Check if the entry has already been added
         if (!game.entryAdded) {
             makeEntry(game.numbers);
-            game.entryAdded = true;
         }
     }
-}
 
-/**
- * Creates a visual entry of numbers in the UI
- */
-function makeEntry(numbers) {
-    numbers.forEach(function(number, index) {
-        if (index === 0) {
-            entry.append(number >= 0 ? number : ' - ' + Math.abs(number));
+    /**
+     * Add numbers to the history panel
+     * @param {Int} numbers 
+     */
+    function makeEntry(numbers) {
+        const entryHtml = numbers.map((number, index) => {
+            return number >= 0 ? (index === 0 ? number : ` + ${Math.abs(number)}`) : ` - ${Math.abs(number)}`;
+        }).join('');
+        entry.append(`${entryHtml}<br />`);
+        game.entryAdded = true;
+    }
+
+    /**
+     * Play the numbers
+     */
+    $('#play').click(function(){
+        game.currentIndex = 0;
+        generateRandomNumbers();
+        updateCurrentSum();
+        game.entryAdded = false;
+        yourAnswer.val('');
+        if(settings.speakNumbers) {
+            displayNumbersSpeak();
         } else {
-            entry.append(number >= 0 ? ' + ' + number : ' - ' + Math.abs(number));
+            game.playnum = setInterval(displayNumbers, settings.timeInterval);
         }
     });
 
-    entry.append('<br />');
-}
+    /**
+     * Replay last numbers
+     */
+    $('#replay').click(function(){
+        game.currentIndex = 0;
+        yourAnswer.val('')
+        if(settings.speakNumbers) {
+            displayNumbersSpeak();
+        } else {
+            game.playnum = setInterval(displayNumbers, settings.timeInterval);
+        }
+    });
 
-/**
- * Play the numbers
- */
-$('#play').click(function(){
-    game.currentIndex = 0;
-    generateRandomNumbers();
-    updateCurrentSum();
-    game.entryAdded = false;
-    yourAnswer.val('');
-    game.playnum = setInterval(displayNumbers, timeInterval);
-});
+    /**
+     * Perform Answer check
+     */
+    $('#check').click(function() {
+        if (game.currentAnswer == yourAnswer.val()) {
+            messageContainer.text(correct);
+            if(settings.speakNumbers) convertSpeech(correct);
+        } else {
+            messageContainer.text(incorrect);
+            if(settings.speakNumbers) convertSpeech(incorrect);
+        }
+    });
 
-/**
- * Replay last numbers
- */
-$('#replay').click(function(){
-    game.currentIndex = 0;
-    updateCurrentSum();
-    yourAnswer.val('')
-    game.playnum = setInterval(displayNumbers, timeInterval);
-});
-
-/**
- * Perform Answer check
- */
-$('#check').click(function() {
-    let yourAns = $('#yourAnswer').val();
-    if (game.currentSum == yourAns) {
-        messageContainer.text(correct);
-        speak(correct);
-    } else {
-        messageContainer.text(incorrect);
-        speak(incorrect);
+    /**
+     * Change Speech voice
+     */
+    let voices = [];
+    speechSynthesis.onvoiceschanged = function() {
+        voices = speechSynthesis.getVoices();
     }
-});
 
-/**
- * Validate settings
- */
-$('#setting_form').validate();
-
-/**
- * Disable settings save or close on invalid
- */
-$('#setting_form input').keyup(function() {
-    let invalid = $('#setting_form input.error');
+    /**
+     * Convert text to speech
+     * @param {str,int} toSpeak 
+     */
+    function convertSpeech(toSpeak) {
+        const speakUtterance = new SpeechSynthesisUtterance();
+        speakUtterance.voice = voices[2];
+        speakUtterance.rate = 1.5;
+        speakUtterance.pitch = 1;
+        speakUtterance.lang = 'en-US'
+        speakUtterance.text = toSpeak;
+        if(settings.speakNumbers){
+            speakUtterance.onend = function() {
+                game.currentIndex++;
+                if (game.currentIndex <= game.numbers.length) {
+                    setTimeout(displayNumbersSpeak, settings.timeInterval);
+                }
+            }
+        }
+        speechSynthesis.speak(speakUtterance);
+    }
     
-    if(invalid.length > 0){
-        $('#settings_save').prop('disabled', true);
-        $('.btn-close').prop('disabled', true);
-    } else {
-        $('#settings_save').prop('disabled', false);
-        $('.btn-close').prop('disabled', false);
-    }
 });
-
-/**
- * Convert text to speech
- * @param {str,int} toSpeak 
- */
-function speak(toSpeak) {
-    const speakUtterance = new SpeechSynthesisUtterance();
-    const voices = speechSynthesis.getVoices();
-    speakUtterance.voice = voices[2];
-    speakUtterance.rate = 1.5;
-    speakUtterance.pitch = 1;
-
-    console.log(voices);
-    speakUtterance.text = toSpeak;
-    speechSynthesis.speak(speakUtterance);
-}
